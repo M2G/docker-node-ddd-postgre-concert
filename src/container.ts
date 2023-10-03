d8cc56376a1c0bd66ffe0d9b504484c84a4381b9
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-10-03 03:09:29 +0200

    feat: up project

--- src/container.ts
+++ src/container.ts
@@ -2,16 +2,16 @@ import { createContainer, asValue, asFunction } from 'awilix';
 
 import app from './app';
 import server from './interfaces/http/server';
+import router from './interfaces/http/router';
 import auth from './interfaces/http/auth';
 import verify from './interfaces/http/middlewares/verify';
 import config from '../config';
 import jwt from './infra/jwt/jwt';
 import redis from './infra/redis/caching';
 import logger from './infra/logging/logger';
+import response from './infra/support/response';
 import repository from './infra/repositories';
 import database from './infra/database';
-import schema from './interfaces/schema-definition';
-import response from './infra/support/response';
 
 const container = createContainer();
 
@@ -25,7 +25,7 @@ container.register({
   redis: asFunction(redis).singleton(),
   repository: asFunction(repository).singleton(),
   response: asFunction(response).singleton(),
-  schema: asFunction(schema).singleton(),
+  router: asFunction(router).singleton(),
   server: asFunction(server).singleton(),
   verify: asFunction(verify).singleton(),
 });
--- src/interfaces/http/server.ts
+++ src/interfaces/http/server.ts
@@ -1,64 +1,20 @@
-import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
-import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
-import { ApolloServerPluginCacheControl } from '@apollo/server/plugin/cacheControl';
-import { ApolloServerPluginLandingPageGraphQLPlayground } from '@apollo/server-plugin-landing-page-graphql-playground';
-import { startStandaloneServer } from '@apollo/server/standalone';
 import express from 'express';
-import cors from 'cors';
-import http from 'http';
-import { json } from 'body-parser';
 
-export default ({ config, logger, auth, schema, verify }: any) => {
+export default ({ config, router, logger, auth }: any) => {
   const app = express();
 
-  const httpServer = http.createServer(app);
-  const apolloServer = new ApolloServer<any>({
-    cache: 'bounded',
-    csrfPrevention: true,
-    introspection: true,
-    plugins: [
-      ApolloServerPluginCacheControl({ defaultMaxAge: 5 }),
-      ApolloServerPluginDrainHttpServer({ httpServer }),
-      ApolloServerPluginLandingPageGraphQLPlayground({}),
-    ],
-    resolvers: schema.resolvers,
-    typeDefs: schema.typeDefs,
-  });
-
-  app.use(
-    cors({
-      credentials: true,
-      origin: true,
-    }),
-  );
   app.disable('x-powered-by');
   app.use(auth.initialize());
-  app.use(auth.authenticate);
+  app.use(router);
 
   return {
-    server: apolloServer,
-    serverStandalone:
-      process.env.NODE_ENV === 'test' &&
-      startStandaloneServer(apolloServer, { listen: config.port }),
     app,
-    start: async (): Promise<unknown> =>
+    start: () =>
       new Promise(() => {
-        if (process.env.NODE_ENV === 'development') {
-          return app.listen(config.port, async () => {
-            console.log('config.port config.port', config.port);
-            await apolloServer.start();
-            app.use(
-              '/graphql',
-              cors(),
-              json(),
-              expressMiddleware(apolloServer, {
-                context: verify.authorization,
-              }),
-            );
-            logger.info(`🚀 Server ready at http://localhost:8181/graphql`);
-          });
-        }
+        const http: any = app.listen(config.port, () => {
+          const { port } = http.address();
+          logger.info(`API - Port ${port}`);
+        });
       }),
   };
 };
